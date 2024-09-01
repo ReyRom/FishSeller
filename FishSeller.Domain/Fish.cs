adefaf86c5c1f5ba22912d3bee7dc67a3774741f
ReyRom <s03roman@mail.ru>
2024-09-01 22:51:28 +0300

    Добавьте файлы проекта.

---
+++ FishSeller.Domain/Fish.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FishSeller.Domain
+{
+    public class Fish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public double Price { get; set; }
+    }
+}
