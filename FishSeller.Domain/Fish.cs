792d887732a7f961dfa808939e8a64e3c911af77
ReyRom <s03roman@mail.ru>
2024-09-02 23:53:02 +0300

    models

--- FishSeller.Domain/Fish.cs
+++ FishSeller.Domain/Fish.cs
@@ -5,7 +5,6 @@ namespace FishSeller.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Amount { get; set; }
         public double Price { get; set; }
     }
 }
---
+++ FishSeller.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿namespace FishSeller.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long TelegramId { get; set; }
+        public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
+    }
+}
